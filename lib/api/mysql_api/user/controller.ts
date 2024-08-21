9fb04c9d83eeab108c1c73b868cd94990f97b7a6
Kowthaman <kandasamykowtham125@gmail.com>
2024-08-21 15:14:34 +0530

    login and register api validation added

--- lib/api/mysql_api/user/controller.ts
+++ lib/api/mysql_api/user/controller.ts
@@ -5,9 +5,15 @@ import { AppFunction } from "../../../core/app";
 import Helper from "./helper";
 import { StringConstant } from "../../../config/constant";
 import {IUser} from "../../../core/interface/user"
+import { validationResult } from "express-validator";
 
 export class UserController {
     public login = async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return badResponse(errors.array(), res);
+        }
+
         const loginResponse = await User.login(req.body.userName);
         if (loginResponse.err) {
           return failureResponse(loginResponse.message, res);
@@ -23,29 +29,34 @@ export class UserController {
         }
   
         return successResponse(Helper.loginResponse(loginResponse.data[0]), "Login successfull", res);
-      };
+    };
 
-      public register = async (req: Request, res: Response) => {
-        const body = req.body as IUser;
-
-        const user = await User.findUserByUsername(body.userName);
-        if (user.err) {
-          return failureResponse(user.message, res);
-        }
-        if(user.data[0].length > 0)
-        {
-          return failureResponse("User already exist", res);
-        }
-        body.code = AppFunction.uuid();
-        body.password = AppFunction.encryptPassword(body.password);
-
-        const register = await User.register(body);
-        if(register.err)
-        {
-          return failureResponse(register.message, res);
-        }
-
-        return successResponse(register.data, "User Registered Successfully", res);
-
+    public register = async (req: Request, res: Response) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return badResponse(errors.array(), res);
       }
+
+      const body = req.body as IUser;
+
+      const user = await User.findUserByUsername(body.userName);
+      if (user.err) {
+        return failureResponse(user.message, res);
+      }
+      if(user.data[0].length > 0)
+      {
+        return failureResponse("User already exist", res);
+      }
+      body.code = AppFunction.uuid();
+      body.password = AppFunction.encryptPassword(body.password);
+
+      const register = await User.register(body);
+      if(register.err)
+      {
+        return failureResponse(register.message, res);
+      }
+
+      return successResponse(register.data, "User Registered Successfully", res);
+
+    }
 }
--- lib/api/mysql_api/user/routes.ts
+++ lib/api/mysql_api/user/routes.ts
@@ -1,5 +1,6 @@
 import { Application, Request, Response } from "express";
 import { UserController } from "./controller";
+import { Validation } from "./class";
 
 export class UserRoutes {
   private userCtrl: UserController = new UserController();
@@ -7,6 +8,7 @@ export class UserRoutes {
   public route(app: Application, url: string) {
     app.post(
       url + "/login",
+      Validation.loginValidation,
       (req: Request, res: Response) => {
         this.userCtrl.login(req, res);
       }
@@ -14,6 +16,7 @@ export class UserRoutes {
 
     app.post(
       url + "/register",
+      Validation.registerValidation,
       (req: Request, res: Response) => {
         this.userCtrl.register(req, res);
       }
