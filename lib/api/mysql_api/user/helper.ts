6e8990031005914e71e428805aa6893b38bde033
Kowthaman <kandasamykowtham125@gmail.com>
2024-09-18 15:57:03 +0530

    Unwanted code lines removed

--- lib/api/mysql_api/user/helper.ts
+++ lib/api/mysql_api/user/helper.ts
@@ -61,34 +61,4 @@ export default class Helper {
     return userInfo;
   }
 
-  public static userRes = (userData: any) => {
-    const userInfo = { ...userData[0][0] };
-    delete userInfo.password;
-    delete userInfo.isDeleted;
-    userInfo.divisionIds = [];
-
-
-    userData[1].forEach((item) => {
-      const existingDivision = userInfo.divisionIds.find(div => div.id === item.divisionId);
-
-      if (existingDivision) {
-          existingDivision.villages.push({
-              id: item.villageId,
-              name: item.villageName
-          });
-      } else {
-          userInfo.divisionIds.push({
-              id: item.divisionId,
-              name: item.name,
-              isDefault: item.isDefault,
-              villages: [{
-                  id: item.villageId,
-                  name: item.villageName
-              }]
-          });
-      }
-  });
-    
-    return userInfo;
-  }
 }
