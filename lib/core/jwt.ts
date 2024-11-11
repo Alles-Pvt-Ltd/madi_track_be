768c54f63d63ec928d16e4ab394394e58209f31e
unknown <kkirisha20@gmail.com>
2024-11-11 07:44:32 +0530

    remove comments

--- lib/core/jwt.ts
+++ lib/core/jwt.ts
@@ -4,17 +4,15 @@ import { AppFunction } from "./app";  // Assuming AppFunction contains jwtVerify
 const jwt = require('jsonwebtoken');
 
 export class JwtToken {
-  // Standard JWT Token Verification
   public static verify(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers['authorization']?.split(' ')[1]; // Extract "Bearer <token>"
+    const token = req.headers['authorization']?.split(' ')[1];
 
     if (!token) {
       return forbidden("Access Denied: No token provided", req.body, res);
     }
 
     try {
-      const verified = AppFunction.jwtVerify(token); // Verifies the token with the secret
-     // Attach the decoded data to req.user for future middleware or request handling
+      const verified = AppFunction.jwtVerify(token); 
     } catch (error) {
       return forbidden("Invalid token provided", req.body, res);
     }
@@ -22,22 +20,22 @@ export class JwtToken {
     return next();
   }
 
-  // Admin-specific JWT Token Verification
+
   public static adminVerify(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers['authorization']?.split(' ')[1]; // Extract "Bearer <token>"
+    const token = req.headers['authorization']?.split(' ')[1]; 
 
     if (!token) {
       return forbidden("Access Denied: No token provided", req.body, res);
     }
 
     try {
-      const verified = AppFunction.jwtVerify(token); // Verifies the token with the secret
+      const verified = AppFunction.jwtVerify(token); 
 
-      // Check if the user has admin role (assuming role is part of the payload)
-      if (verified.email) {  // Assuming role 2 is for admin
+      
+      if (verified.email) {  
         return forbidden("Access Denied: Admins only", req.body, res);
       }
- // Attach the decoded data to req.user for future middleware or request handling
+ 
     } catch (error) {
       return forbidden("Invalid token provided", req.body, res);
     }
@@ -45,16 +43,13 @@ export class JwtToken {
     return next();
   }
 
-  // Function to extract user data from the token (e.g., user code)
   public static get(req: Request): { username: string, email: string } {
-    const token = req.headers['authorization']?.split(' ')[1]; // Extract token from Authorization header
+    const token = req.headers['authorization']?.split(' ')[1]; 
 
     if (!token) {
       throw new Error("Token is required");
     }
-
-    // Return decoded user data from JWT token
-    const userData = AppFunction.jwtVerify(token);  // Using AppFunction.jwtVerify to decode token and get user info
+    const userData = AppFunction.jwtVerify(token);  
     return userData;
   }
 }
